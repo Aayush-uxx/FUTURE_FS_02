a83054fcab08455fb0b8332869ed3ed9e92486af
Aayush <xaayush977@gmail.com>
2026-06-02 18:18:17 +0545

    commit for authController

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import dbCon from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
 dotenv.config();
 dbCon();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
