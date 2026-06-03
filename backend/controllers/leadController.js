c021070bec5217ed05f03e7d7403a84e9f4e0375
Aayush <xaayush977@gmail.com>
2026-06-03 19:10:06 +0545

    commit for api update and delete

--- backend/controllers/leadController.js
+++ backend/controllers/leadController.js
@@ -31,4 +31,48 @@ const getAllLead = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export { createLead, getAllLead };
+
+const getLeadById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const leads = await leadModel.findById(id);
+    if (!leads) return res.status(404).json({ message: "No leads found !" });
+    res.status(200).json(leads);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const updateLead = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+    const validStatus = ["new", "contacted", "converted"];
+    if (!validStatus.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid status. Must be: new, contacted, or converted",
+      });
+    }
+    const lead = await leadModel.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
+    if (!lead)
+      return res.status(404).json({ message: "No lead found for Update !" });
+    res.status(200).json({ message: "Status updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const deleteLead = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteLead = await leadModel.findByIdAndDelete(id);
+    if (!deleteLead)
+      return res.status(404).json({ message: "No Lead found to delete !" });
+    res.status(200).json({ message: "Lead deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export { createLead, getAllLead, getLeadById, updateLead, deleteLead };
--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -1,6 +1,15 @@
-import { createLead, getAllLead } from "../controllers/leadController.js";
+import {
+  createLead,
+  deleteLead,
+  getAllLead,
+  getLeadById,
+  updateLead,
+} from "../controllers/leadController.js";
 import express from "express";
 const router = express.Router();
 router.post("/createLead", createLead);
 router.get("/getAllLead", getAllLead);
+router.get("/getLeadById/:id", getLeadById);
+router.put("/updateLead/:id", updateLead);
+router.delete("/deleteLead/:id", deleteLead);
 export default router;
